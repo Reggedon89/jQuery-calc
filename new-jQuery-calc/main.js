cb50464d5f23f67b04da9bd9d87b0cee2f5bf4e7
Geoff Hutson <geoff.hutson.89@gmail.com>
2019-07-10 19:39:28 -0700

    added more stuff

--- new-jQuery-calc/main.js
+++ new-jQuery-calc/main.js
@@ -7,13 +7,41 @@ $(document).ready(() => {
   };
   // this displays the actual number pressed on calculator screen
   function inputNum(digit) {
-    const { displayValue } = calculator;
+    const { displayValue, waitingForSecondOperator } = calculator;
     // Overwrite the display value if there is nothing in it, otherwise append to it
     // pull displavalue out of calculator in order to use it as a variable
     //Is 0 a digit, if so change displayVale to that digit
-    calculator.displayValue =
-      displayValue === "0" ? digit : displayValue + digit;
+    //the second operator now overwrites instead of appending to to it
+    if (waitingForSecondOperator === true) {
+      calculator.displayValue = digit;
+      calculator.waitingForSecondOperator = false;
+    } else {
+      calculator.displayValue =
+        displayValue === "0" ? digit : displayValue + digit;
+    }
+
+    console.log(calculator);
   }
+  //This function creates a decimal point
+  function inputDec(dot) {
+    if (!calculator.displayValue.includes(dot)) {
+      calculator.displayValue += dot;
+    }
+    console.log(calculator);
+  }
+  function handleOperator(nextOperator) {
+    //destructure calculator and pull our what is needed
+    const { firstOperator, displayValue, operator } = calculator;
+    const inputValue = parseFloat(displayValue);
+
+    if (firstOperator === null) {
+      calculator.firstOperator = inputValue;
+    }
+
+    calculator.waitingForSecondOperator = true;
+    calculator.operator = nextOperator;
+  }
+
   //changes the display of calculator()
   function display() {
     const updateDisplay = document.querySelector(".calculator-screen");
@@ -30,13 +58,18 @@ $(document).ready(() => {
     if (!target.matches("button")) {
       return;
     }
-    if ($(target).hasClass("operator")) {
+    if (target.classList.contains("operator")) {
+      handleOperator(target.value);
+      display();
+    }
+
+    if (target.classList.contains("decimal")) {
+      inputDec(target.value);
+      display();
       return;
     }
-    if ($(target).hasClass("decimal")) {
-      return;
-    }
-    if ($(target).hasClass("all-clear")) {
+
+    if (target.classList.contains("all-clear")) {
       return;
     }
     //When the target is clicked and these functions update the calculator screen
